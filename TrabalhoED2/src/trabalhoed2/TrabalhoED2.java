fd2f8004a6ef44acce0bf9314ab79bfa41312bd5
Anisio Santos <anisio.dev@gmail.com>
2017-10-07 18:26:53 -0300

    Problemas com acentos resolvidos, algumas partes do codigo viraram classes, ja funciona. Iniciando comentarios de auxilio

--- TrabalhoED2/src/trabalhoed2/TrabalhoED2.java
+++ TrabalhoED2/src/trabalhoed2/TrabalhoED2.java
@@ -6,38 +6,46 @@ import java.util.*;
 import java.util.logging.*;
 
 public class TrabalhoED2 {
-    public static String arrumarTextoParaCorte(String text) { 
-            return text.replaceAll("[ãâàáä]", "a")   
-	                .replaceAll("[êèéë]", "e")   
-	                .replaceAll("[îìíï]", "i")   
-	                .replaceAll("[õôòóö]", "o")   
-	                .replaceAll("[ûúùü]", "u")     
-	                .replace('ç', 'c')   
-	                .replace('ñ', 'n')
-                        .replace(",","")
-                        .replace(".","")
-                        .replace(":","")
-                        .replace(";","")
-                        .replace("!","")
-                        .replace("\"","")
-                        .replace("\'","")
-                        .replace("\\","");
+
+    public static String arrumarSinais(String texto) { 
+        return texto.replaceAll("[ãâàáä]", "a")   
+                    .replaceAll("[êèéë]", "e")   
+	            .replaceAll("[îìíï]", "i")   
+	            .replaceAll("[õôòóö]", "o")   
+	            .replaceAll("[ûúùü]", "u")     
+	            .replace('ç', 'c')   
+	            .replace('ñ', 'n');
     }   
+    
+    public static String tirarPontuacao(String texto){
+        return texto.replace(",","")
+                    .replace(".","")
+                    .replace(":","")
+                    .replace(";","")
+                    .replace("!","")
+                    .replace("\"","")
+                    .replace("\'","")
+                    .replace("\\","");
+    }
     
     public static void trocarComMenor(String [] palavrasUnicas, int menorPalavra, int palavraDaVez){
         String palavraAux = new String();
         palavraAux = palavrasUnicas[menorPalavra];
         palavrasUnicas[menorPalavra] = palavrasUnicas[palavraDaVez];
-        palavrasUnicas[palavraDaVez] = palavraAux; 
+        palavrasUnicas[palavraDaVez] = palavraAux;        
+        //palavraDaVez++;               
     }
     
-    public static int procurarMenor(String [] palavrasUnicas, int menorPalavra,int palavraDaVez){        
-        for ( int i = palavraDaVez ; i < (palavrasUnicas.length-1) ; i ++ ){
-            if ( palavrasUnicas[i].compareTo( palavrasUnicas[menorPalavra] ) < 0 ){
-                menorPalavra = i;                    
+    public static int procurarMenor(String [] palavrasUnicas, int menorPalavra, int palavraDaVez){        
+            for ( int i = palavraDaVez ; i < (palavrasUnicas.length-1) ; i ++ ){
+                if ( palavrasUnicas[i].compareTo( palavrasUnicas[menorPalavra] ) < 0 ){
+                    //System.out.println("entrou if: " + palavrasUnicas[i] + " e "+ palavrasUnicas[i+1]);
+                    menorPalavra = i;                    
+                }
             }
-        }          
-        return menorPalavra;
+            
+            //System.out.println(menorPalavra);            
+            return menorPalavra;
     }
     
     public static void main(String[] args) {        
@@ -55,23 +63,12 @@ public class TrabalhoED2 {
             int palavrasIguais = 0;
             
             System.out.println(texto);
-            /*
-            texto = texto.replace(",","");
-            texto = texto.replace(".","");
-            texto = texto.replace(":","");
-            texto = texto.replace(";","");
-            texto = texto.replace("!","");
-            texto = texto.replace("\"","");
-            texto = texto.replace("\'","");
-            texto = texto.replace("\\","");
-            //*/
-            texto = arrumarTextoParaCorte(texto);
-            
+            texto = tirarPontuacao(texto);
             String textoDividido[] = texto.split(" ");
             
             
             System.out.println(Array.getLength(textoDividido));         
-            
+            // tirar palavras repetidas
             String palavrasUnicas[] = new String[textoDividido.length];
             int quantidade = 0;
             for (String textoDividido1 : textoDividido) {
@@ -88,14 +85,44 @@ public class TrabalhoED2 {
                 }              
             }
             
-            
-            
             palavrasUnicas = Arrays.copyOf( palavrasUnicas , quantidade );
             
+            String armazenarTodasPalavras[] = new String[(palavrasUnicas.length * 2)]; 
+            String palavrasUnicasComSinal[] = new String [ quantidade ];
+            palavrasUnicasComSinal = Arrays.copyOf( palavrasUnicas , quantidade );
+            
+            for( int i = 0 ; i < palavrasUnicasComSinal.length ; i++ ){
+                palavrasUnicas [i] = arrumarSinais(palavrasUnicas[i]); 
+                //System.out.println(palavrasUnicasComSinal[i]);                
+            }
+            
+            int j = 0;
+            for ( int i = 0 ; i < armazenarTodasPalavras.length ; i+=2, j++  ){                
+                armazenarTodasPalavras[i] = palavrasUnicas[j];                   
+            }
+            j=0;
+            for ( int i = 1 ; i < armazenarTodasPalavras.length ; i+=2, j++ ){
+                armazenarTodasPalavras[i] = palavrasUnicasComSinal[j];                 
+            }
+            j=0;
+            for (int i = 0 ; i < armazenarTodasPalavras.length ; i++){
+                //System.out.println(armazenarTodasPalavras[i]);
+            }
+           
+            for ( int i = 0 ; i < armazenarTodasPalavras.length ; i+=2 , j++ ){               
+                if ( ! palavrasUnicas[j].equals(palavrasUnicasComSinal[j]) ){
+                    armazenarTodasPalavras[i] = armazenarTodasPalavras[i].concat("a");
+                    palavrasUnicas[j] = palavrasUnicas[j].concat("a");
+                    //System.out.println(palavrasUnicas[j]);
+                }
+            }
+            //*
             
             
-            System.out.println("numero de palavas: " + palavrasUnicas.length);
-            System.out.println("palavras iguais: " + palavrasIguais);
+            
+            //*/
+            //System.out.println("numero de palavas: " + palavrasUnicas.length);
+            //System.out.println("palavras iguais: " + palavrasIguais);
             
             
                         
@@ -103,10 +130,9 @@ public class TrabalhoED2 {
             
             int menorPalavra = 0;
             int palavraDaVez = 0;
-            
-            while ( palavraDaVez != palavrasUnicas.length ){  
-                /*
+            while ( palavraDaVez != palavrasUnicas.length ) {  
                 //Procurando menor palavra
+                /*
                 for ( int i = palavraDaVez ; i < (palavrasUnicas.length-1) ; i ++ ){
                 //System.out.println("entroufor");
                     if ( palavrasUnicas[i].compareTo( palavrasUnicas[menorPalavra] ) < 0 ){
@@ -122,22 +148,37 @@ public class TrabalhoED2 {
                 palavraAux = palavrasUnicas[menorPalavra];
                 palavrasUnicas[menorPalavra] = palavrasUnicas[palavraDaVez];
                 palavrasUnicas[palavraDaVez] = palavraAux;         
-                palavraDaVez++;
                 //*/
-                //*
                 menorPalavra = procurarMenor(palavrasUnicas,menorPalavra,palavraDaVez);
-                
-                //System.out.println("Menor Palavra: " + palavrasUnicas [ menorPalavra]);             
-               
-                trocarComMenor(palavrasUnicas,menorPalavra,palavraDaVez);
+                //////////System.out.println("Menor Palavra: " + palavrasUnicas [ menorPalavra]);
+                trocarComMenor(palavrasUnicas,menorPalavra,palavraDaVez); 
                 palavraDaVez++;
-                //*/
                 
+            } 
+            for ( int i = 0 ; i < armazenarTodasPalavras.length ; i+=2){
+                for ( int z = 0 ; z < palavrasUnicas.length ; z++){
+                    j = i;
+                    j++;
+                    if((armazenarTodasPalavras[i].equals(palavrasUnicas[z]))&&(!(armazenarTodasPalavras[i].equals(armazenarTodasPalavras[j])))){                       
+                        palavrasUnicas[z] = armazenarTodasPalavras[j];                       
+                    }
+                }
             }
+            
+            for(int i = 0 ; i < palavrasUnicas.length ; i++){
+                System.out.println(palavrasUnicas[i]);
+            }
+            
+            //for ( int i = 0 ; i < armazenar ){
+                
+            //}            
+            
+            //System.out.println("menor : "+menorPalavra);
+            //System.out.println("palavra da vez: " + palavraDaVez);
             //System.out.println("menor palavra de todas: " + palavrasUnicas[menorPalavra]);  
                    
             //*/
-            //*
+            /*
             for ( int i = 0 ; i < palavrasUnicas.length ; i++ )
                 palavras.add(i,palavrasUnicas[i]);
              
@@ -145,8 +186,18 @@ public class TrabalhoED2 {
                 System.out.println(palavras.get(i));
             //*/
             //pular linha .newLine();
-            //escreverArquivoResposta.write(texto);            
-            //escreverArquivoResposta.flush();
+            escreverArquivoResposta.write("Texto com "+textoDividido.length+" palavras.");
+            escreverArquivoResposta.newLine();
+            escreverArquivoResposta.write(palavrasIguais+" palavras repetidas.");
+            escreverArquivoResposta.newLine();
+            escreverArquivoResposta.write("Lista de palavras (Em ordem alfabetica): ");
+            escreverArquivoResposta.newLine();
+            for(int i = 0; i < palavrasUnicas.length ; i++){               
+                escreverArquivoResposta.write(palavrasUnicas[i]); 
+                escreverArquivoResposta.newLine();
+                escreverArquivoResposta.flush();
+            }
+            
             
         } catch (IOException ex) {
             Logger.getLogger(TrabalhoED2.class.getName()).log(Level.SEVERE, null, ex);
